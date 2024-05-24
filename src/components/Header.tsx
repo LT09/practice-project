90003866b474ba17d1c9552597591931e01e941b
Suphanat Natthakul <littlestrike09@gmail.com>
2024-05-24 16:27:19 +0700

    [EDIT] Header responsive

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,6 +3,9 @@
 import React, { FC } from "react";
 import { Button } from "@nextui-org/react";
 import { jsonplaceholderUsersTypes } from "@/types/jsonplaceholderTypes";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPen } from "@fortawesome/free-solid-svg-icons";
+import ProfileImage from "./ProfileImage";
 interface HeaderProps {
   title: string;
   usersData: jsonplaceholderUsersTypes[] | undefined;
@@ -10,20 +13,34 @@ interface HeaderProps {
 
 const Header: FC<HeaderProps> = ({ title, usersData }) => {
   return (
-    <div className="bg-red-500">
-      <h1 className="text-5xl font-bold">{title}</h1>
+    <div className="bg-red-500 lg:p-8 p-4">
+      <h1 className="text-5xl font-bold pb-6">{title}</h1>
       {usersData ? (
-        <ul>
-          {usersData.map((user) => (
-            <li key={user.id}>{user.name}</li>
-          ))}
-        </ul>
+        <div className="container mx-auto flex flex-col md:flex-row items-center justify-between space-y-4 md:space-y-0 md:space-x-6 p-4">
+          <ProfileImage />
+          <div className="text-center md:text-left">
+            <p className="text-4xl font-bold">{usersData[0].name}</p>
+            <p className="text-xl">
+              {usersData[0].company.catchPhrase} @ {usersData[0].company.name}
+            </p>
+            <p className="text-lg">Email: {usersData[0].email}</p>
+            <p className="text-lg">Phone: {usersData[0].phone}</p>
+            <p className="text-lg font-light">
+              City: {usersData[0].address.city}
+            </p>
+            <Button
+              className="my-3 w-full"
+              color="primary"
+              radius="sm"
+              startContent={<FontAwesomeIcon icon={faPen} />}
+            >
+              Edit profile
+            </Button>
+          </div>
+        </div>
       ) : (
         <p>Loading...</p>
       )}
-      <Button color="primary" radius="lg">
-        Button
-      </Button>
     </div>
   );
 };

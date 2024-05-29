b38365260d5e7160f9681492fe746c160743d929
Suphanat Natthakul <littlestrike09@gmail.com>
2024-05-29 14:41:03 +0700

    [ADD] jsonplaceholderCommentsTypes API

--- src/types/jsonplaceholderTypes.ts
+++ src/types/jsonplaceholderTypes.ts
@@ -5,6 +5,14 @@ export type jsonplaceholderPostsTypes = {
   body: string;
 };
 
+export type jsonplaceholderCommentsTypes = {
+  postId: number;
+  id: number;
+  name: string;
+  email: string;
+  body: string;
+};
+
 export type jsonplaceholderPhotosTypes = {
   albumId: number;
   id: number;
